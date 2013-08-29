e75c26a748c7b3d02daac04a090a66509a483bf2
Luo Tao <lotreal@gmail.com>
2013-08-29 11:22:17 +0800

    xxoo

--- protected/config/dev-config.php
+++ protected/config/dev-config.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'I_DB_CONN', 'mysql:host=localhost;dbname=yukuai_itools' );
+define( 'I_DB_CONN', 'mysql:host=192.168.0.123;dbname=yukuai_itools' );
 define( 'I_DB_USER', 'ykdb01' );
 define( 'I_DB_PASS', 'yk2012' );
